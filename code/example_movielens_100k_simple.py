1522a81c4216c7623ce182d95fe1a7761fec8b29
fuhang <1341414814@qq.com>
2019-04-26 15:22:11 +0800

    test gpu

--- code/example_movielens_100k_simple.py
+++ code/example_movielens_100k_simple.py
@@ -20,8 +20,9 @@ if __name__=='__main__':
     ls=[]
     model=LR
     for _ in range(10):
-        #model=FM(features_sizes)
-        model=DeepFM(features_sizes,deep_layers=(10,10),k=10)
+        model=FM(features_sizes)
+        #model = LR(features_sizes)
+        #model=DeepFM(features_sizes,deep_layers=(10,10),k=10)
         best_score=model.fit(train[features]-1,test[features]-1,y_train,y_test,lr=0.0005,N_EPOCH=150,batch_size=500,early_stopping_rounds=30)
         #-1是因为ids要从0起.而数据中是从1起的
         ls.append(best_score)
